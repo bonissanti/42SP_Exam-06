d05bb67c70e0383f52532e4dcfd1365f268ed1b6
Bruno Onissanti <brunoonissanti@gmail.com>
2025-01-07 17:27:34 -0300

    feat: new training of the day. Now the dilemma is if I need to use sizeof or strlen to strings in bzero.

--- mini_serv.c
+++ mini_serv.c
@@ -8,7 +8,6 @@
 void ft_putstr(char *str)
 {
     int i = 0;
-
     while (str[i])
     {
         write(2, &str[i], 1);
@@ -31,7 +30,7 @@ typedef struct s_client
 
 int id = 0;
 int maxFd = 0;
-char recBuf[65632];
+char recvBuf[65632];
 t_client client[1024];
 fd_set readFds, writeFds, currentFds;
 
@@ -40,14 +39,11 @@ void send_to_all(int exceptFd)
     for (int i = 0; i <= maxFd; i++)
     {
         if (FD_ISSET(i, &writeFds) && i != exceptFd)
-        {
-            if (send(i, recBuf, strlen(recBuf), 0) < 0)
-                ft_putstr("Fatal error\n");
-        }
+            send(i, recvBuf, sizeof(recvBuf), 0);
     }
 }
 
-void broadcast_message(t_client client[1024], char buffer[65532], ssize_t len, int fd)
+void broadcast_message(t_client client[1024], char buffer[65532], int fd, ssize_t len)
 {
     for (int i = 0; i < len; i++)
     {
@@ -55,9 +51,9 @@ void broadcast_message(t_client client[1024], char buffer[65532], ssize_t len, i
         if (client[fd].msg[i] == '\n')
         {
             client[fd].msg[i] = '\0';
-            sprintf(recBuf, "client %d: %s\n", client[fd].id, client[fd].msg);
+            sprintf(recvBuf, "client %d: %s\n", client[fd].id, client[fd].msg);
             send_to_all(fd);
-            bzero(&recBuf, strlen(recBuf));
+            bzero(&recvBuf, strlen(recvBuf));
             bzero(&client[fd].msg, strlen(client[fd].msg));
         }
     }
@@ -66,8 +62,7 @@ void broadcast_message(t_client client[1024], char buffer[65532], ssize_t len, i
 int main(int argc, char **argv)
 {
     char buffer[65532];
-    int server_fd = 0;
-    struct sockaddr_in server;
+    struct sockaddr_in server = {0};
 
     if (argc != 2)
     {
@@ -75,33 +70,30 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    memset(&server, 0, sizeof(server));
     memset(&client, 0, sizeof(t_client));
-
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    const int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     server.sin_family = AF_INET;
     server.sin_port = htons(atoi(argv[1]));
     server.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    if (bind(server_fd, (struct sockaddr *)&server, sizeof(server) < 0))
+    if (bind(server_fd, (struct sockaddr *)&server, sizeof(server)) < 0)
     {
         ft_putstr("Fatal error\n");
         close(server_fd);
-        return 1;
+        exit(1);
     }
-
     if (listen(server_fd, 10) < 0)
     {
         ft_putstr("Fatal error\n");
         close(server_fd);
-        return 1;
+        exit(1);
     }
+    maxFd = server_fd;
     FD_ZERO(&readFds);
     FD_ZERO(&writeFds);
     FD_ZERO(&currentFds);
-    FD_SET(server_fd, &currentFds);
-    maxFd = server_fd;
 
+    FD_SET(server_fd, &currentFds);
     while (1)
     {
         writeFds = currentFds;
@@ -110,7 +102,7 @@ int main(int argc, char **argv)
         {
             ft_putstr("Fatal error\n");
             close(server_fd);
-            return 1;
+            exit(1);
         }
         for (int i = 0; i <= maxFd; i++)
         {
@@ -119,30 +111,33 @@ int main(int argc, char **argv)
                 if (i == server_fd)
                 {
                     socklen_t len = sizeof(server);
-                    const int client_fd = accept(server_fd, (struct sockaddr *)&server, &len);
+                    int client_fd = accept(server_fd, (struct sockaddr *)&server, &len);
                     if (client_fd < 0)
                     {
-                        ft_putstr("Fatal error\n");
+                        ft_putstr("Fatal error\n"); // Maybe don't need to be printed
                         continue;
                     }
                     client[client_fd].id = id++;
                     maxFd = max(maxFd, client_fd);
                     FD_SET(client_fd, &currentFds);
-                    sprintf(recBuf, "server: client %d just arrived\n", client[client_fd].id);
+                    sprintf(recvBuf, "server: client %d just arrived\n", client[client_fd].id);
                     send_to_all(client_fd);
-                    bzero(&recBuf, strlen(recBuf));
+                    bzero(&recvBuf, strlen(recvBuf));
+                    break;
                 }
-                ssize_t len = recv(i, buffer, strlen(buffer), 0);
+                const ssize_t len = recv(i, buffer, sizeof(buffer), 0);
                 if (len <= 0)
                 {
-                    sprintf(recBuf, "server: client %d just left\n", client[i].id);
+                    sprintf(recvBuf, "server: client %d just left\n", client[i].id);
                     send_to_all(i);
                     FD_CLR(i, &currentFds);
                     close(i);
-                    bzero(&recBuf, strlen(recBuf));
+                    bzero(&recvBuf, strlen(recvBuf));
+                    break ;
                 }
                 buffer[len] = '\0';
-                broadcast_message(client, buffer, len, i);
+                broadcast_message(client, buffer, i, len);
+                bzero(&buffer, sizeof(buffer));
             }
         }
     }
