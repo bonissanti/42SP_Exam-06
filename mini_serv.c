9af361b22c0f8097de257ced63c1dabf9fae635d
Bruno Onissanti <brunoonissanti@gmail.com>
2025-01-07 13:05:56 -0300

    feat: finished and look like ok

--- mini_serv.c
+++ mini_serv.c
@@ -24,9 +24,17 @@ typedef struct s_client
 
 int maxFd = 0;
 int id = 0;
+char buffer[65532];
 char revBuf[65632];
 fd_set readFds, writeFds, currentFds;
 t_client client[1024];
+
+int max(int a, int b)
+{
+    if (a > b)
+        return a;
+    return b;
+}
 
 void send_to_all(int exceptFd)
 {
@@ -39,6 +47,22 @@ void send_to_all(int exceptFd)
                 ft_putstr("Fatal error\n");
                 exit(1);
             }
+        }
+    }
+}
+
+void broadcast_message(t_client client[1024], char buffer[65532], ssize_t len, int fd)
+{
+    for (int i = 0; i < len; i++)
+    {
+        client[fd].msg[i] = buffer[i];
+        if (client[fd].msg[i] == '\n')
+        {
+            client[fd].msg[i] = '\0';
+            sprintf(revBuf, "client %d: %s\n", client[fd].id, client[fd].msg);
+            send_to_all(fd);
+            bzero(&client[fd].msg, strlen(client[fd].msg));
+            bzero(&revBuf, strlen(revBuf));
         }
     }
 }
@@ -79,7 +103,7 @@ int main(int argc, char **argv)
     FD_ZERO(&readFds);
     FD_ZERO(&writeFds);
     FD_ZERO(&currentFds);
-    FD_ISSET(server_fd, &currentFds);
+    FD_SET(server_fd, &currentFds);
     maxFd = server_fd;
     while(1)
     {
@@ -88,13 +112,13 @@ int main(int argc, char **argv)
         if (select(maxFd + 1, &readFds, &writeFds, NULL, NULL) < 0)
         {
             ft_putstr("Fatal error\n");
-            close(server_fd);
-            FD_CLR(server_fd, &currentFds);
+            // close(server_fd);
+            // FD_CLR(server_fd, &currentFds);
             exit(1);
         }
         for (int i = 0; i <= maxFd; i++)
         {
-            if (FD_ISSET(server_fd, &readFds))
+            if (FD_ISSET(i, &readFds))
             {
                 if (i == server_fd)
                 {
@@ -103,16 +127,28 @@ int main(int argc, char **argv)
                     if (client_fd < 0)
                     {
                         ft_putstr("Fatal error\n");
-                        break ;
+                        continue ;
                     }
+                    maxFd = max(maxFd, client_fd);
                     client[client_fd].id = id++;
                     FD_SET(client_fd, &currentFds);
                     sprintf(revBuf, "server: client %d just arrived\n", client[client_fd].id);
                     send_to_all(client_fd);
                     bzero(&revBuf, strlen(revBuf));
+                    break ;
                 }
+                ssize_t len = recv(i, buffer, sizeof(buffer), 0);
+                if (len <= 0 )
+                {
+                    sprintf(revBuf, "server: client %d just left\n", client[i].id);
+                    send_to_all(i);
+                    FD_CLR(i, &currentFds);
+                    close(i);
+                    bzero(&revBuf, strlen(revBuf));
+                }
+                buffer[len] = '\0';
+                broadcast_message(client, buffer, len, i);
             }
         }
-
     }
 }
