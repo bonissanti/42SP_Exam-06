c770f20f3e09fc73e03c2cf72726818f485f98f4
Bruno Onissanti <brunoonissanti@gmail.com>
2025-01-18 21:04:20 -0300

    feat: exame06 corrigido

--- mini_serv.c
+++ mini_serv.c
@@ -1,9 +1,10 @@
 #include <unistd.h>
-#include <sys/socket.h>
 #include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <stdio.h>
 
 void ft_putstr(char *str)
 {
@@ -15,7 +16,7 @@ void ft_putstr(char *str)
     }
 }
 
-int max(int a, int b)
+int getMax(int a, int b)
 {
     if (a > b)
         return a;
@@ -30,31 +31,32 @@ typedef struct s_client
 
 int id = 0;
 int maxFd = 0;
-char recvBuf[65632];
 t_client client[1024];
-fd_set readFds, writeFds, currentFds;
+char send_buffer[65632];
+char recv_buffer[65532];
+fd_set write_fds, read_fds, current_fds;
 
 void send_to_all(int exceptFd)
 {
     for (int i = 0; i <= maxFd; i++)
     {
-        if (FD_ISSET(i, &writeFds) && i != exceptFd)
-            send(i, recvBuf, sizeof(recvBuf), 0);
+        if (FD_ISSET(i, &write_fds) && i != exceptFd)
+            send(i, send_buffer, strlen(send_buffer), 0);
     }
 }
 
-void broadcast_message(t_client client[1024], char buffer[65532], int fd, ssize_t len)
+void broadcast_message(t_client client[1024], char recv_buffer[65532], int fd, ssize_t len)
 {
-    size_t j = strlen(client[fd].msg);
+    int j = strlen(client[fd].msg);
     for (int i = 0; i < len; i++, j++)
     {
-        client[fd].msg[j] = buffer[i];
+        client[fd].msg[j] = recv_buffer[i];
         if (client[fd].msg[j] == '\n')
         {
             client[fd].msg[j] = '\0';
-            sprintf(recvBuf, "client %d: %s\n", client[fd].id, client[fd].msg);
+            sprintf(send_buffer, "client %d: %s\n", client[fd].id, client[fd].msg);
             send_to_all(fd);
-            bzero(&recvBuf, strlen(recvBuf));
+            bzero(&send_buffer, strlen(send_buffer));
             bzero(&client[fd].msg, strlen(client[fd].msg));
             j = -1;
         }
@@ -63,8 +65,7 @@ void broadcast_message(t_client client[1024], char buffer[65532], int fd, ssize_
 
 int main(int argc, char **argv)
 {
-    char buffer[65532];
-    struct sockaddr_in server = {0};
+    struct sockaddr_in server;
 
     if (argc != 2)
     {
@@ -72,74 +73,86 @@ int main(int argc, char **argv)
         return 1;
     }
 
+    memset(&server, 0, sizeof(server));
     memset(&client, 0, sizeof(t_client));
-    const int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    server.sin_family = AF_INET;
-    server.sin_port = htons(atoi(argv[1]));
+
     server.sin_addr.s_addr = htonl(INADDR_ANY);
+    server.sin_port = htons(atoi(argv[1]));
+    server.sin_family = AF_INET;
+
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
     if (bind(server_fd, (struct sockaddr *)&server, sizeof(server)) < 0)
     {
         ft_putstr("Fatal error\n");
         close(server_fd);
-        exit(1);
+        return 1;
     }
+
     if (listen(server_fd, 10) < 0)
     {
         ft_putstr("Fatal error\n");
         close(server_fd);
-        exit(1);
+        return 1;
     }
-    maxFd = server_fd;
-    FD_ZERO(&readFds);
-    FD_ZERO(&writeFds);
-    FD_ZERO(&currentFds);
 
-    FD_SET(server_fd, &currentFds);
+    FD_ZERO(&write_fds);
+    FD_ZERO(&read_fds);
+    FD_ZERO(&current_fds);
+
+    FD_SET(server_fd, &current_fds);
+    maxFd = server_fd;
+
     while (1)
     {
-        writeFds = currentFds;
-        readFds = currentFds;
-        if (select(maxFd + 1, &readFds, &writeFds, NULL, NULL) < 0)
+        read_fds = current_fds;
+        write_fds = current_fds;
+
+        if (select(maxFd + 1, &read_fds, &write_fds, NULL, NULL) < 0)
         {
             ft_putstr("Fatal error\n");
             close(server_fd);
-            exit(1);
+            FD_CLR(server_fd, &current_fds);
+            return 1;
         }
+
         for (int i = 0; i <= maxFd; i++)
         {
-            if (FD_ISSET(i, &readFds))
+            if (FD_ISSET(i, &read_fds))
             {
                 if (i == server_fd)
                 {
                     socklen_t len = sizeof(server);
                     int client_fd = accept(server_fd, (struct sockaddr *)&server, &len);
                     if (client_fd < 0)
-                    {
-                        ft_putstr("Fatal error\n"); // Maybe don't need to be printed
                         continue;
-                    }
+                    maxFd = getMax(maxFd, client_fd);
+                    FD_SET(client_fd, &current_fds);
                     client[client_fd].id = id++;
-                    maxFd = max(maxFd, client_fd);
-                    FD_SET(client_fd, &currentFds);
-                    sprintf(recvBuf, "server: client %d just arrived\n", client[client_fd].id);
+                    sprintf(send_buffer, "server: client %d just arrived\n", client[client_fd].id);
                     send_to_all(client_fd);
-                    bzero(&recvBuf, strlen(recvBuf));
+                    bzero(&send_buffer, strlen(send_buffer));
                     break;
                 }
-                const ssize_t len = recv(i, buffer, sizeof(buffer), 0);
-                if (len <= 0)
+                else
                 {
-                    sprintf(recvBuf, "server: client %d just left\n", client[i].id);
-                    send_to_all(i);
-                    FD_CLR(i, &currentFds);
-                    close(i);
-                    bzero(&recvBuf, strlen(recvBuf));
-                    break ;
+                    ssize_t len = recv(i, recv_buffer, sizeof(recv_buffer), 0);
+                    if (len <= 0)
+                    {
+                        bzero(&send_buffer, strlen(send_buffer));
+                        sprintf(send_buffer, "server: client %d just left\n", client[i].id);
+                        send_to_all(i);
+                        FD_CLR(i, &current_fds);
+                        close(i);
+                        bzero(&client[i].msg, strlen(client[i].msg));
+                        break;
+                    }
+                    else
+                    {
+                        recv_buffer[len] = '\0';
+                        broadcast_message(client, recv_buffer, i, len);
+                    }
                 }
-                buffer[len] = '\0';
-                broadcast_message(client, buffer, i, len);
-                bzero(&buffer, sizeof(buffer));
             }
         }
     }
